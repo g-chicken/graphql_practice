001be6bdb92a31e7d5443300ccd103276effb543
g-chicken <398ah16@gmail.com>
2022-07-10 18:07:16 +0900

    first commit

---
+++ golang/graph/schema.resolvers.go
@@ -0,0 +1,63 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/g-chicken/graphql_practice/golang/graph/generated"
+	"github.com/g-chicken/graphql_practice/golang/graph/model"
+)
+
+// CreateLink is the resolver for the createLink field.
+func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
+	link := &model.Link{
+		Address: input.Address,
+		Title:   input.Title,
+		User: &model.User{
+			Name: "test",
+		},
+	}
+
+	return link, nil
+}
+
+// CreateUser is the resolver for the createUser field.
+func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Login is the resolver for the login field.
+func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// RefreshToken is the resolver for the refreshToken field.
+func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Links is the resolver for the links field.
+func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	links := []*model.Link{
+		{
+			ID:      "1",
+			Title:   "our dummy link",
+			Address: "https://address.org",
+			User:    &model.User{Name: "admin"},
+		},
+	}
+
+	return links, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
